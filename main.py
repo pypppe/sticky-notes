7e75e832fc7a7ad6ef2cd66782359d6bd46aac5a
pyp <bobdirmanner@gmail.com>
2025-10-15 21:39:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from window import create_main_window
 from settings import open_settings
 from helpers import save_text, load_text
 
-# globals
 BG_COLOR = "#fff48c"
 CURRENT_FONT = ("Segoe UI", 11)
 
@@ -22,14 +21,13 @@ def on_close():
     save_text(text, BG_COLOR, CURRENT_FONT)
     root.destroy()
 
-# --- create window ---
 root, text = create_main_window(on_close, lambda: open_settings(root, text, BG_COLOR, CURRENT_FONT, set_bg_color, set_font))
 
-# --- load saved content properly ---
 saved_bg, saved_font = load_text(text)
 BG_COLOR = saved_bg
 CURRENT_FONT = saved_font
-text.config(bg=BG_COLOR, font=CURRENT_FONT)  # update text widget with loaded values
+text.config(bg=BG_COLOR, font=CURRENT_FONT)
 
 root.protocol("WM_DELETE_WINDOW", on_close)
 root.mainloop()
+
